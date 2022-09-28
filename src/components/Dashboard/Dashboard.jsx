e6ddac2234515240dfe86df1e623d9f52021b7fe
kathrynzaro <zarokathryn@gmail.com>
2022-09-27 17:09:51 -0700

    adds form and form styles to Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,7 +3,50 @@ import styles from './Dashboard.css';
 export default function Dashboard() {
   return (
     <div className={styles.Dashboard}>
-      <h1>Dashboard</h1>
+      <form>
+
+        <label>
+          Name
+          <input name="name" placeholder="Your name" />
+        </label>
+        
+        <label>
+          Date
+          <input type="date" name="date" />
+        </label>
+
+        <label>
+          Option
+          <select>
+            <option disabled selected value="">
+              Select an option...
+            </option>
+            <option value="1">A</option>
+            <option value="2">B</option>
+            <option value="3">C</option>
+          </select>
+        </label>
+
+        <label>
+          Bio
+          <textarea name="bio" placeholder="tell us about yourself" />
+        </label>
+
+        <fieldset>
+          <legend>Do you accept?</legend>
+
+          <label>
+            I accept
+            <input type="checkbox" />
+          </label>
+
+          <label>
+            I do not accept
+            <input type="checkbox" />
+          </label>
+        </fieldset>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
